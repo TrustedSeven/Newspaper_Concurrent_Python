56c76ed8d15731a80a02b91fcdffe0269d7623ad
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-03-13 22:10:37 -0500

    install module

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import newspaper from newspaper 
-import Article
+import timeit
+import newspaper
+from newspaper import Article
+
 
 def get_headlines():
 
 
-URLs = ['http://www.foxnews.com/',
-'http://www.cnn.com/',
-'http://www.derspiegel.de/',
-'http://www.bbc.co.uk/',
-'https://theguardian.com',]
-
+    URLs = ['http://www.foxnews.com/',
+        'http://www.cnn.com/',
+        'http://www.derspiegel.de/',
+        'http://www.bbc.co.uk/',
+        'https://theguardian.com',]
 
 
 for url in URLs:
-result = newspaper.build(url, memoize_articles=False)
-print('\n''The headlines from %s are' % url, '\n')
-for i in range(1,6):
-art = result.articles[i]
-art.download()
-art.parse()
-print(art.title)
-
+    result = newspaper.build(url, memoize_articles=False)
+    print('\n''The headlines from %s are' % url, '\n')
+for i in range(1, 6):
+    art = result.articles[i]
+    art.download()
+    art.parse()
+    print(art.title)
 
 
 if name == '__main__':
-import timeit
-elapsed_time = timeit.timeit("get_headlines()", setup="from main import get_headlines", number=2)/2
-print(elapsed_time)
+    elapsed_time = timeit.timeit(
+    "get_headlines()", setup="from main import get_headlines", number=2)/2
+    print(elapsed_time)
